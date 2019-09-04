261a4834e3fc9bcfebffd7d8ecc6c54afc6fdf93
Eric Vincent <eric.vincent@jet.com>
2019-09-04 01:34:13 -0400

    look at the tokens, also comments

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,47 @@
-extern crate pest;
-#[macro_use]
-extern crate pest_derive;
-
+use pest;
+use pest_derive::*;
 use pest::Parser;
 
 #[derive(Parser)]
-#[grammar="csv.pest"]
+#[grammar = "csv.pest"]
 pub struct CSVParser;
 
 fn main() {
     println!("current working directory: {:?}", std::env::current_dir());
-    
+
     let f = std::fs::read_to_string("numbers.csv").expect("cannot read file");
-    let file = CSVParser::parse(Rule::file, &f)
-        .expect("unsuccessful parse")
-        .next().unwrap();
-    
+    // the parse result at the topmost level is rule `file`, who's span is 
+    // the whole file, and it's inner list is of all the record types in the file.
+    // This is why `pr.clone().next()` is a list of pairs.
+    // NOTE: the clne of the parse result is needed because the later 
+    // `parse_result.token()` would consume the parse result and not allow the 
+    // file to be used later in the code.
+    let (parse_result, file) = {
+        let pr = CSVParser::parse(Rule::file, &f).expect("unsuccessful parse");
+        println!("{:?}", pr);
+        let f = pr.clone().next().unwrap();
+        (pr, f)
+    };
+
+    // The parse result's "lowest" form are pairs of tokens that mark the
+    // start and end position of the matched rule in the source material.
+    // This can be useful but is also burdensome.
+    let tokens = parse_result.tokens();
+    println!(" tokens\n--------");
+    for t in tokens {
+        println!("{:?}", t);
+    }
+
     let mut field_sum: f64 = 0.0;
     let mut record_count: u64 = 0;
 
-    for record in file.into_inner() {
+    // `file` is 
+    for record in file.into_inner() {   // outermost rule is the file. This
+                                        // returns the iterator of the children
+                                        // of the current pair. It could have 
+                                        // been called children_iterator. In
+                                        // this case, child of file is the
+                                        // records, or lines of the csv
         match record.as_rule() {
             Rule::record => {
                 record_count += 1;
